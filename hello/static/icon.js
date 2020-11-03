a7a0b0733ba9dfb0b43b9c18e0addd09648b90bb
lokesh <lokeshkolte2580@gmail.com>
2020-11-03 23:05:39 +0530

    changes

--- hello/static/icon.js
+++ hello/static/icon.js
@@ -3,10 +3,10 @@ document.onreadystatechange = function() {
         document.querySelector( 
           "body").style.visibility = "hidden"; 
         document.querySelector( 
-          "#loader").style.visibility = "visible"; 
+          "#icon").style.visibility = "visible"; 
     } else { 
         document.querySelector( 
-          "#loader").style.display = "none"; 
+          "#icon").style.display = "none"; 
         document.querySelector( 
           "body").style.visibility = "visible"; 
     } 

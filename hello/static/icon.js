bea76a3bc8c79ca814506d5763455daa06ede572
lokesh <lokeshkolte2580@gmail.com>
2020-11-05 15:23:03 +0530

    d

--- hello/static/icon.js
+++ hello/static/icon.js
@@ -1,13 +1,13 @@
 document.onreadystatechange = function() { 
-    if (document.readyState !== "complete") { 
-        document.querySelector( 
-          "body").style.visibility = "hidden"; 
-        document.querySelector( 
-          "#icon").style.visibility = "visible"; 
-    } else { 
-        document.querySelector( 
-          "#icon").style.display = "none"; 
-        document.querySelector( 
-          "body").style.visibility = "visible"; 
-    } 
+  if (document.readyState !== "complete") { 
+      document.querySelector( 
+        "body").style.visibility = "hidden"; 
+      document.querySelector( 
+        "#icon").style.visibility = "visible"; 
+  } else { 
+      document.querySelector( 
+        "#icon").style.display = "none"; 
+      document.querySelector( 
+        "body").style.visibility = "visible"; 
+  } 
 }; 
